6d59292ed1b17af607aca4c12d12580fb11017ce
thaissalaury <laurythaissa@gmail.com>
2026-05-12 10:04:07 -0300

    incrementei o placeholder nos InputField

--- src/components/FormularioCadastro.jsx
+++ src/components/FormularioCadastro.jsx
@@ -1,14 +1,17 @@
 import InputField from "./inputField";
+import BotaoEnviar from "./BotaoEnviar";
 
 function FormularioCadastro() {
     return (
         <div>
-            <InputField label="Nome: " type="text" name="nome" />
-            <InputField label="Email: " type="email" name="email" />
-            <InputField label="idade: " type="number" name="number"/>
-            <InputField label="data de nascimento: " type="date" name="date"/>
-            <InputField label="Cidade: " type="text" name="cidade"/>
-            <InputField label="Estado: " type="text" name="estado"/>
+            <InputField label="Nome: " type="text" name="nome" placeholder="Digite seu nome"/>
+            <InputField label="Email: " type="email" name="email" placeholder="email@empresa.com"/>
+            <InputField label="Idade: " type="number" name="number" placeholder="19anos"/>
+            <InputField label="Cidade: " type="text" name="cidade" placeholder="cidade que mora"/>
+            <InputField label="Estado: " type="text" name="estado" placeholder="estado que mora"/>
+            <InputField label="Telefone" type="text" name="telefone" placeholder="(79)99999-9999"/>
+            <InputField label= "Gênero" type="select" name="Feminino " placeholder="Feminino ou Masculino"/>
+            <BotaoEnviar texto="cadastrar"/>
         </div>
     )
 }
